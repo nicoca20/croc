79ecdd4ff07da344f99eca49d1d1522dbb54fa18
Nico <nico.github@canzani.ch>
2025-05-23 18:32:09 +0200

    switch pulser checkout in bender again. Test this pulser version in helloworld.c.

--- sw/helloworld.c
+++ sw/helloworld.c
@@ -36,55 +36,53 @@ int main()
     pulser0_settings.invert_out = 0;
     pulser0_settings.idle_high = 0;
 
-    *reg32(PULSER_BASE_ADDR, PULSER_F1_CFG_REG_OFFSET) = 15;
+    pulser_settings_t pulser1_settings;
+    pulser1_settings.f1_end = 8;
+    pulser1_settings.f1_switch = 4;
+    pulser1_settings.f2_end = 6;
+    pulser1_settings.f2_switch = 2;
+    pulser1_settings.f1_count = 1;
+    pulser1_settings.f2_count = 9;
+    pulser1_settings.stop_count = 3;
+    pulser1_settings.invert_out = 0;
+    pulser1_settings.idle_high = 0;
 
-    // pulser_settings_t pulser1_settings;
-    // pulser1_settings.f1_end = 8;
-    // pulser1_settings.f1_switch = 4;
-    // pulser1_settings.f2_end = 6;
-    // pulser1_settings.f2_switch = 2;
-    // pulser1_settings.f1_count = 1;
-    // pulser1_settings.f2_count = 9;
-    // pulser1_settings.stop_count = 3;
-    // pulser1_settings.invert_out = 0;
-    // pulser1_settings.idle_high = 0;
+    pulser_settings_t pulser2_settings;
+    pulser2_settings.f1_end = 9;
+    pulser2_settings.f1_switch = 5;
+    pulser2_settings.f2_end = 7;
+    pulser2_settings.f2_switch = 3;
+    pulser2_settings.f1_count = 2;
+    pulser2_settings.f2_count = 4;
+    pulser2_settings.stop_count = 6;
+    pulser2_settings.invert_out = 0;
+    pulser2_settings.idle_high = 0;
 
-    // pulser_settings_t pulser2_settings;
-    // pulser2_settings.f1_end = 9;
-    // pulser2_settings.f1_switch = 5;
-    // pulser2_settings.f2_end = 7;
-    // pulser2_settings.f2_switch = 3;
-    // pulser2_settings.f1_count = 2;
-    // pulser2_settings.f2_count = 4;
-    // pulser2_settings.stop_count = 6;
-    // pulser2_settings.invert_out = 0;
-    // pulser2_settings.idle_high = 0;
+    pulser_settings_t pulser3_settings;
+    pulser3_settings.f1_end = 6;
+    pulser3_settings.f1_switch = 2;
+    pulser3_settings.f2_end = 8;
+    pulser3_settings.f2_switch = 5;
+    pulser3_settings.f1_count = 7;
+    pulser3_settings.f2_count = 1;
+    pulser3_settings.stop_count = 9;
+    pulser3_settings.invert_out = 0;
+    pulser3_settings.idle_high = 0;
 
-    // pulser_settings_t pulser3_settings;
-    // pulser3_settings.f1_end = 6;
-    // pulser3_settings.f1_switch = 2;
-    // pulser3_settings.f2_end = 8;
-    // pulser3_settings.f2_switch = 5;
-    // pulser3_settings.f1_count = 7;
-    // pulser3_settings.f2_count = 1;
-    // pulser3_settings.stop_count = 9;
-    // pulser3_settings.invert_out = 0;
-    // pulser3_settings.idle_high = 0;
+    pulser_config(PULSER_0, &pulser0_settings);
+    pulser_config(PULSER_1, &pulser1_settings);
+    pulser_config(PULSER_2, &pulser2_settings);
+    pulser_config(PULSER_3, &pulser3_settings);
 
-    // pulser_config(PULSER_0, &pulser0_settings);
-    // pulser_config(PULSER_1, &pulser1_settings);
-    // pulser_config(PULSER_2, &pulser2_settings);
-    // pulser_config(PULSER_3, &pulser3_settings);
+    // Test Pulser 0
 
-    // // Test Pulser 0
-
-    // pulser_start(1 << PULSER_0);
-    // pulser_stop(1 << PULSER_0);
-    // for (int i = 0; i < 2; i++)
-    // {
-    //     __asm__ volatile("nop");
-    // }
-    // pulser_start(1 << PULSER_0);
+    pulser_start(1 << PULSER_0);
+    pulser_stop(1 << PULSER_0);
+    for (int i = 0; i < 2; i++)
+    {
+        __asm__ volatile("nop");
+    }
+    pulser_start(1 << PULSER_0);
 
     // Test Pulser 1
 
@@ -122,13 +120,13 @@ int main()
     //     __asm__ volatile("nop");
     // }
 
-    // pulser_start((1 << PULSER_0) | (1 << PULSER_1) | (1 << PULSER_2) | (1 << PULSER_3));
-    // pulser_stop((1 << PULSER_0) | (1 << PULSER_1) | (1 << PULSER_2) | (1 << PULSER_3));
-    // for (int i = 0; i < 2; i++)
-    // {
-    //     __asm__ volatile("nop");
-    // }
-    // pulser_start((1 << PULSER_0) | (1 << PULSER_1) | (1 << PULSER_2) | (1 << PULSER_3));
+    pulser_start((1 << PULSER_0) | (1 << PULSER_1) | (1 << PULSER_2) | (1 << PULSER_3));
+    pulser_stop((1 << PULSER_0) | (1 << PULSER_1) | (1 << PULSER_2) | (1 << PULSER_3));
+    for (int i = 0; i < 2; i++)
+    {
+        __asm__ volatile("nop");
+    }
+    pulser_start((1 << PULSER_0) | (1 << PULSER_1) | (1 << PULSER_2) | (1 << PULSER_3));
 
     // // Check pulser reading functions
     // // printf("Pulser state fsm: 0x%x\n", get_pulser_fsm_state(PULSER_TO_TEST));
@@ -150,12 +148,12 @@ int main()
     // // printf("Pulser state fsm: 0x%x\n", get_pulser_fsm_state(PULSER_TO_TEST));
     // // uart_write_flush();
 
-    // // Read Signature from ROM
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     printf("Rom data: %x\n", *reg32(USER_ROM_BASE_ADDR, i * 4));
-    //     uart_write_flush();
-    // }
+    // Read Signature from ROM
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Rom data: %x\n", *reg32(USER_ROM_BASE_ADDR, i * 4));
+        uart_write_flush();
+    }
 
     return 1;
 }
