018fe6985d5cd38d8ef4cc69223455a05cb2d14e
Nico <nico.github@canzani.ch>
2025-05-29 14:28:53 +0200

    Update Adv timer base address. Was in user space before, is in croc space now

--- sw/config.h
+++ sw/config.h
@@ -14,7 +14,7 @@
 #define GPIO_BASE_ADDR 0x03005000
 #define TIMER_BASE_ADDR 0x0300A000
 #define USER_ROM_BASE_ADDR 0x20000000
-#define ADV_TIMER_BASE_ADDR 0x20001000
+#define ADV_TIMER_BASE_ADDR 0x0300E000
 #define PULSER_BASE_ADDR 0x0300C000
 
 // Frequencies
@@ -29,9 +29,10 @@
 
 // Since SRAM is very limmited, select which part to compile and test.
 // Difficult to impossible to activate more than one test
-#define TEST_READ_ROM           0
-#define TEST_REG_PART_F1        0
-#define TEST_REG_PART_F2        0
-#define TEST_REG_PART_CNT       0
-#define TEST_RUN_ALL_PULSERS    1
-#define TEST_RUN_ADV_TIMER      0
+#define TEST_READ_ROM               0
+#define TEST_REG_PART_F1            0
+#define TEST_REG_PART_F2            0
+#define TEST_REG_PART_CNT           0
+#define TEST_RUN_ALL_PULSERS        0
+#define TEST_RUN_PULSER_ONE_BY_ONE  0
+#define TEST_RUN_ADV_TIMER          1
